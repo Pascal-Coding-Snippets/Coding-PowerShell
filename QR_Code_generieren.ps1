39fbf3213bc76ca358df0437939976dea9c22ef9
Pascal-Coding-Snippets@github.com <pascal.aebischer@bluetec.ch>
2025-02-21 14:11:54 +0100

    update

--- QR_Code_generieren.ps1
+++ QR_Code_generieren.ps1
@@ -1,5 +1,13 @@
+
+# Set-ExecutionPolicy setzen
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+
+# Modul installieren
 Install-Module -Name QRCodeGenerator
-$MY_URL = 'https://www.bluetec.ch'
+
+# Variablen definieren
+$MY_URL = 'https://www.google.com'
 $DESTINATION_FILE = 'C:\URL-QR.PNG'
+
+# QR-Code generieren
 New-PSOneQRCodeURI -URI $MY_URL -Width 15 -OutPath $DESTINATION_FILE
