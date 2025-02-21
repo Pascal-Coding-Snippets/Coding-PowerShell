7816c7cf05314e546140ff2d149b6839fa604f66
Pascal-Coding-Snippets@github.com <pascal.aebischer@bluetec.ch>
2025-02-21 20:32:38 +0100

    update

--- Supporting_and_Troubleshooting_Win11.ps1
+++ Supporting_and_Troubleshooting_Win11.ps1
@@ -1,5 +1,19 @@
 
-# Aus Überblick Windows PowerShell
+## Allgemeine Befehle ##
+
+# Installation PowerShell
+winget install --id Microsoft.PowerShell --source winget
+
+# Aus Folie "Überblick Windows PowerShell"
 Get-Service -Name "A*" | Where-Object {$_. Status -eq "Running"}
 
+## ExecutionPolicy prüfen ##
 
+# Aktuelle Übersicht
+Get-ExecutionPolicy -List
+
+# Siehe Skriptausführung / Speccs
+Set-ExecutionPolicy -ExecutionPolicy [Spezifikation]
+
+# Siehe Input 2
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force
